d4624f523632e83ab9f069e41f73422caecc2b1d
Ehsan <ehsan.farzadnia@gmail.com>
2026-04-08 11:13:55 +0200

    savetocsv.m

--- cod/mtlb/savetocsv.m
+++ cod/mtlb/savetocsv.m
@@ -8,28 +8,9 @@ for i=1:83
     [a,mu,sigma] = zscore(ds{i,1}(:,6:17));
     T = array2table(a, 'VariableNames', colnames);
     S = array2table(dfnc_ts{i,4}(:,6:17),'VariableNames',colnames);
-
+    
     % assign target labels to training folds
-
-    %T{:,18} = targets{[1:i-1, i+1:83],2}; % hallucinatory behavior
-    %T{:,19} = targets{[1:i-1, i+1:83],3}; % hallucinatory behavior (categorized)
-    %T{:,20} = targets{[1:i-1, i+1:83],5}; % unusual thoughts content
-    %T{:,21} = targets{[1:i-1, i+1:83],6}; % unusual thoughts content (categorized)
-    %T{:,22} = targets{[1:i-1, i+1:83],8}; % unusual thoughts content
-    %T{:,23} = targets{[1:i-1, i+1:83],9}; % unusual thoughts content (categorized)    
-
-    % assign target labels to testing folds
-
-    %S{:,18} = targets{i,2}; % hallucinatory behavior
-    %S{:,19} = targets{i,3}; % hallucinatory behavior (categorized)
-    %S{:,20} = targets{i,5}; % unusual thoughts content
-    %S{:,21} = targets{i,6}; % unusual thoughts content (categorized)
-    %S{:,22} = targets{i,8}; % unusual thoughts content
-    %S{:,23} = targets{i,9}; % unusual thoughts content (categorized)   
-
-
-    %% 
-
+    
     T{:,13} = targets{[1:i-1, i+1:83],2}; % hallucinatory behavior
     T{:,14} = targets{[1:i-1, i+1:83],3}; % hallucinatory behavior (categorized)
     T{:,15} = targets{[1:i-1, i+1:83],5}; % unusual thoughts content
@@ -48,8 +29,8 @@ for i=1:83
     S{:,18} = targets{i,9}; % unusual thoughts content (categorized)  
     S{:,19} = targets{i,10}; % unusual thoughts content (categorized)  
 
-
     writetable(T, ['trn_',num2str(i),'.csv']);
     writetable(S, ['tst_',num2str(i),'.csv']);
+
 end
 
